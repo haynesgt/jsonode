4485654f12d4b06c627c8765c595dbbea21d13a9
Gavin Haynes <haynes.gt@gmail.com>
2021-07-28 11:37:40 -0700

    fix: handle streaming input

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const vm = require("vm");
 const minimist = require("minimist");
 const console = require("console");
 
+function help() {
+  console.error(`Usage:\n` +
+    `  jsonode [(-r|--require) module]* [--raw-in] [--raw-out] [-u|--ugly] [-i|--indent=2] ((-f|--file) <file.js> | <code>)`,
+    `  <command with json output> | jsonode <code>\n` +
+    `  jsonode <code> < <json file>\n\n` +
+    `  jsonode --file <file.js> < <json file>\n\n` +
+    `Runs the code as javascript, where \`$\` contains input parsed as json. ` +
+    `The output is converted to json by default. ` +
+    `If the output is a function, it is called with the input as the argument. ` +
+    `If the output is a promise, it is awaited. \n\n` +
+    `Options:\n`
+    `  --ugly\tsuppress indented output\n` +
+    `  --raw-in\tmake \`$\` equal the input as a string rather than json\n`
+    `  --raw-out\tprint the output as a string rather than json\n`
+    `  --require\tinclude a module and make it available by its name\n`
+  );
+}
+
 function argToArray(arg) {
   if (typeof arg === Array) {
     return arg;
@@ -26,6 +44,12 @@ function argToString(arg) {
   return arg && arg.toString();
 }
 
+async function read(stream) {
+  const chunks = [];
+  for await (const chunk of stream) chunks.push(chunk);
+  return Buffer.concat(chunks).toString('utf8');
+}
+
 (async () => {
   try {
     const argOpts = {
@@ -35,27 +59,13 @@ function argToString(arg) {
     const args = minimist(process.argv.slice(2), argOpts);
 
     if (args.help) {
-      console.error(`Usage:\n` +
-        `  jsonode [(-r|--require) module]* [--raw-in] [--raw-out] [-u|--ugly] [-i|--indent=2] ((-f|--file) <file.js> | <code>)`,
-        `  <command with json output> | jsonode <code>\n` +
-        `  jsonode <code> < <json file>\n\n` + 
-        `  jsonode --file <file.js> < <json file>\n\n` + 
-        `Runs the code as javascript, where \`$\` contains input parsed as json. ` + 
-        `The output is converted to json by default. ` +
-        `If the output is a function, it is called with the input as the argument. ` +
-        `If the output is a promise, it is awaited. \n\n` +
-        `Options:\n`
-        `  --ugly\tsuppress indented output\n` +
-        `  --raw-in\tmake \`$\` equal the input as a string rather than json\n`
-        `  --raw-out\tprint the output as a string rather than json\n`
-        `  --require\tinclude a module and make it available by its name\n`
-      );
+      help();
       process.exit(1);
     }
 
-    const code = args.file ? fs.readFileSync(args.file) : argToString(args._);
+    const code = args.file ? fs.readFileSync(args.file) : argToString(args._) || '$';
 
-    const input = fs.readFileSync(0).toString();
+    const input = await read(process.stdin);
     const $ = args['raw-in'] ? input : JSON.parse(input);
     const modules = argToArray(args.require).reduce((accum, next) => ({[next.replace(/-/g, "_")]: require(next)}), {})
     const context = vm.createContext({$, require, ...modules});
